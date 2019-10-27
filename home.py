a47abbd233dead7a7e8369065645ad6c938dcd58
Zhuo Wen <53234730+Stanley9733@users.noreply.github.com>
2019-10-27 17:54:16 -0500

    2nd update

--- home.py
+++ home.py
@@ -2,10 +2,15 @@ from flask import Flask
 from flask import Blueprint, redirect, render_template, request, url_for, jsonify
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/',methods=['GET', 'POST'])
 def hello():
+    if request.method == 'POST':
+        data = request.form.to_dict(flat=True)
+        name = data['username']
+        return redirect('/table/{}'.format(name))
     return render_template("homepage.html")
 
-@app.route('/table')
-def showinfo():
-    return render_template("table.html")
+@app.route('/table/<id>')
+def showinfo(id):
+    showname = id
+    return render_template("Employee home.html", showname = showname)
