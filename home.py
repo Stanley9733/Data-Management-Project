1f8a7576e01511879d638c7a9ef8bdb2cd272d2d
Zhuo Wen <53234730+Stanley9733@users.noreply.github.com>
2019-10-28 10:23:54 -0500

    3rd update

--- home.py
+++ home.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask import Blueprint, redirect, render_template, request, url_for, jsonify
 app = Flask(__name__)
+app.debug = True
 
 @app.route('/',methods=['GET', 'POST'])
 def hello():
@@ -10,7 +11,20 @@ def hello():
         return redirect('/table/{}'.format(name))
     return render_template("homepage.html")
 
-@app.route('/table/<id>')
+@app.route('/table/<id>',methods=['GET', 'POST'])
 def showinfo(id):
+    if request.method == 'POST':
+        move = request.form['submit']
+        if move == 'Check Points':
+            return redirect('/redeem/{}'.format(id))
+        if move == 'Give Points':
+            return redirect("https://www.youtube.com/")
+        if move == 'Redeem Points':
+            return redirect("http://www.baidu.com")
     showname = id
     return render_template("Employee home.html", showname = showname)
+
+
+@app.route('/redeem/<id>',methods=['GET', 'POST'])
+def redeem(id):
+    return render_template("Redeem.html")
